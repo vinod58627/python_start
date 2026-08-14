33c2a833ec2c47abbf864279dfb01b2c1168ba1e
vinod-herb-ui <vinod.b@apcfss.in>
2026-08-14 11:47:25 +0530

    Added Variable with Print functions and format, f-string

---
+++ basic_concepts/p_01_variables.py
@@ -0,0 +1,17 @@
+print("Hello World")
+a,b,c = 20,30, 40
+d = a+b+c
+print(d)
+print(('the sum of {} , {} and {} is = {}').format(a,b,c,d))
+print(f'the sum of {a} , {b} and {c} is = {d}')
+
+x = 10
+y = "vinod"
+z = True
+w = 1+2j
+m = 17.7
+
+
+print(x,y,z,w, m)
+
+print(type(x), type(y), type(z), type(w), type(m))
