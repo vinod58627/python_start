08544fe13e72ae855cda3ceb7fe71252e42471ab
vinod-herb-ui <vinod.b@apcfss.in>
2026-08-14 18:18:25 +0530

    String methods Continues

---
+++ basic_concepts/p_04_string_methods.py
@@ -0,0 +1,13 @@
+
+#String Indexing : s[staet:end:step]
+#=====================
+ # 0,1,2,3,.................n Right or Forward Index
+ #-1,-2,-3,.............-n Left or Backward Indexl̥
+s1 = "Python is dynamically typed language"
+
+print(s1.upper()) #PYTHON IS DYNAMICALLY TYPED LANGUAGE
+print(s1.lower()) #python is dynamically typed language
+
+print(s1[0]) #P
+print(s1[-1]) #e
+print(s1[0:5]) #Pytho
