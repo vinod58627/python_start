2c16872debf35557b10c224f59d6965dff237961
vinod-herb-ui <vinod.b@apcfss.in>
2026-08-14 18:29:06 +0530

    Added String methods

--- basic_concepts/p_04_string_methods.py
+++ basic_concepts/p_04_string_methods.py
@@ -11,3 +11,12 @@ print(s1.lower()) #python is dynamically typed language
 print(s1[0]) #P
 print(s1[-1]) #e
 print(s1[0:5]) #Pytho
+
+print(s1[-16: -3]) #y typed langu
+print(s1[-25::]) #ynamically typed language
+
+print(s1[:])#Python is dynamically typed language
+
+print(s1[-30: 30]) #is dynamically typed la
+print(s1[-30: 30: 2]) #sdnmclytpdl
+print(s1[::2])#Pto sdnmclytpdlnug
