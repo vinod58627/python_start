bf32c6519b8e94b254caa42e7ccaa4893b00847f
vinod-herb-ui <vinod.b@apcfss.in>
2026-09-07 13:03:05 +0530

    List methods are completed

--- data_structures/ds_02_list.py
+++ data_structures/ds_02_list.py
@@ -2,17 +2,44 @@
 # List is one of data structure
 # List is mutable
 # list accepts index and slicingepts duplicate values
+# way to create list
+# Method	Description
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
 
+
+# 1.
+#=====================
 x = [10,20,30,40,50,60]
-print(x)
-print(type(x))
+print(x) #[10, 20, 30, 40, 50, 60]
+print(type(x)) #<class 'list'>
+
+# 2. Using list() constructor
+#=============================
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist) #['apple', 'banana', 'cherry']
+print(type(thislist)) #<class 'list'>
 
 # List is mutable means we can change the values
 print(x[2]) #30
 
 x[2] = 25
-print(x) #<class 'list'>
+print(x) #[10, 20, 25, 40, 50, 60]
 print(x[2]) #25
+
+#Change multiple values in the list
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
 
 # list accepts duplicate values
 x = [10,20,30,40,50,60,30,30,-23]
@@ -155,9 +182,51 @@ print(e) # [70, 66, 50, 44, 30, 25, 20, 10, 10]
 e.reverse()
 print(e) # [10, 10, 20, 25, 30, 44, 50, 66, 70]
 
-# 9. Clear: 
+# 9. Clear: clear the values but still keeps the empty list itself
 #==============================================
 f = [10, 20, 30, 44, 50, 66, 70, 25, 10]
 f.clear()
 print(f) #[] clear the values  not the list still its a empty list
 print(type(f)) # <class 'list'>
+
+# 10. Slicing:[start:stop:step] start is include & stop is excluded in slicing
+#==============================================
+
+g = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(type(g))
+print(g[:]) #[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(g[::]) #[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(g[::2]) #[10, 30, 50, 70, 90]
+
+print(g[1:5]) #[20, 30, 40, 50]
+print(g[::-1]) # [100, 90, 80, 70, 60, 50, 40, 30, 20, 10] reverse the list
+print(g[::-2]) # [100, 80, 60, 40, 20] take from reverse index
+
+print(g[3:]) # [40, 50, 60, 70, 80, 90, 100] take staritng from 3rd index
+print(g[3:6]) # [40, 50, 60] 3rd index to 5th index end index is excluded 
+print(g[2:7]) # [30, 40, 50, 60, 70]
+
+print(g[-2:-6]) #[] because by default index moves 0->1->2
+# so here -2 -> -1-> 0 so wont take, so
+
+print(g[-6:-2]) #[50, 60, 70, 80]
+
+print(g[-6:-2:2]) #[50, 70]
+
+# Check the value is in list or not
+
+thislist = ["apple", "banana", "cherry"]
+
+if "cherry" in thislist:
+    print("Yes, 'cherry' is in the list")
+else:
+    print("No, 'cherry' is not in the list")
+    
+#Yes, 'cherry' is in the list
+    
+if "vinod" in thislist:
+    print("Yes, 'vinod' is in the list")
+else:
+    print("No, 'vinod' is not in the list")
+    
+#No, 'vinod' is not in the list
